7e95e9af8376cf189a4db36b9f17287b887a8f42
imanelamrani <drlamrani13@gmail.com>
2025-01-30 21:53:54 -0500

    changed config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://drlamrani.github.io',
-  integrations: [mdx(), sitemap(), tailwind()],
+  //integrations: [mdx(), sitemap(), tailwind()],
   outDir: 'public',
   publicDir: 'static',
 });
