5950bb678904b8d661e83e9c83930b40c2f67054
imanelamrani <drlamrani13@gmail.com>
2025-01-30 20:06:26 -0500

    2nd commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://drlamrani.github.io',
-  base: "/main",
   outDir: 'public',
   publicDir: 'static',
 });
